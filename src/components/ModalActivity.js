946dc2898bfb965dc71796282c4560bb40a422a2
Gigih Hadi <hadigigih@gmail.com>
2023-01-16 22:31:14 +0700

    add activity and delete activity

---
+++ src/components/ModalActivity.js
@@ -0,0 +1,107 @@
+import axiosConfig from '../services/axiosConfig';
+import * as React from 'react';
+
+const ModalActivity = (props) => {
+    const {
+        idActivity,
+        isShowModal,
+        setSumClicked
+    } = props
+
+    const [title, setTitle] = React.useState('');
+
+    React.useEffect(() => {
+        axiosConfig.get('/activity-groups/'+idActivity)
+            .then(response => {return response.data})
+            .then(resp => {setTitle(resp.title)})
+    },[]);
+
+    const handleConfirmDelete = () => {
+        axiosConfig.delete('/activity-groups/'+idActivity,{})
+            .then(response => {return response.data})
+
+        isShowModal(false)
+        setSumClicked(Math.random())
+    }
+
+    return (
+        <>
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
+                    {/* <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                        <label className="text-lg font-semibold">{title}</label>
+                        <button onClick={closeModal}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 stroke-gray-400">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div> */}
+
+                    {/*body*/}
+                    <div 
+                        className="
+                        p-6 flex-auto mt-6
+                        items-center justify-center"
+                    >
+                        <div className="justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-28 h-28 stroke-red-500 text-center items-center">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                            </svg>
+                            Apakah anda yakin ingin menghapus activity <strong>"{title}"</strong>
+                        </div>
+                    </div>
+                    
+                    {/*footer*/}
+                    <div className="flex items-center p-6 rounded-b content-center">
+                        <button
+                            className="
+                                bg-slate-200
+                                rounded-full
+                                font-bold 
+                                text-sm 
+                                mx-2
+                                px-8 py-4
+                                outline-none 
+                                focus:outline-none 
+                                ease-linear transition-all duration-150
+                            "
+                            onClick={() => isShowModal(false)}
+                        >
+                            Batal
+                        </button>
+                        <button 
+                            type="button"
+                            className="
+                                bg-red-500 
+                                text-white 
+                                rounded-full
+                                active:bg-emerald-600 
+                                font-bold 
+                                text-sm 
+                                mx-2
+                                px-8 py-4
+                                outline-none 
+                                focus:outline-none 
+                                ease-linear transition-all duration-150"
+                            onClick={handleConfirmDelete}
+                        >
+                            Hapus
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div 
+            className="opacity-25 fixed inset-0 z-40 bg-black"
+            onClick={() => isShowModal(false)}
+        >
+        </div>
+    </>
+    )
+}
+
+export default ModalActivity;
--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,25 +1,74 @@
 import * as React from 'react';
 import Navbar from '../components/Navbar';
-import InfoPage from '../components/InfoPage';
+import Title from '../components/Title';
 import Card from '../components/Card';
 import axiosConfig from '../services/axiosConfig';
 import moment from 'moment';
 import 'moment/locale/id';
+import ModalActivity from '../components/ModalActivity';
 
 const Activities = () => {
     const [activities, setActivities] = React.useState([])
+    const [sumClicked, setSumClicked] = React.useState(0);
+    const [showModal, setShowModal] = React.useState(false);
+    const [IDActivity, setIDActivity] = React.useState('');
 
     React.useEffect(() => {
         axiosConfig.get('/activity-groups')
             .then(response => {return response.data.data})
             .then(resp => {setActivities(resp)})
-    },[]);
+    },[sumClicked]);
+
+    const handleClickAdd = () => {
+        let clicked = sumClicked + 1;
+
+        const data = {title: 'New Activity'}
+        axiosConfig.post('/activity-groups',data)
+            .then(response => {return response.data.data})
+            .then(resp => {setSumClicked(clicked)})
+    }
 
     return(
         <>
             <Navbar />
+            {showModal?
+                <ModalActivity
+                    isShowModal = {setShowModal}
+                    idActivity = {IDActivity}
+                    setSumClicked = {setSumClicked}
+                />
+                :
+                <></>
+            }
             <div className='px-14'>
-                <InfoPage titlePage='Activity' showAddBtn={true}/>
+                <div className='container grid grid-cols-4 py-4' data-cy="info-page">
+                    <div className='col-start-1 col-span-2 inline-flex'>
+                        <Title script='Activity'/>
+                    </div>
+                    <div className='col-span-2 relative'>
+                        <button 
+                            className="
+                                rounded-full 
+                                float-right 
+                                px-5 py-2.5 
+                                text-center text-white 
+                                inline-flex 
+                                items-center 
+                                mr-2 
+                                leading-tight 
+                                transition duration-150 ease-in-out 
+                                bg-sky-500" 
+                            type="button" 
+                            data-cy="activity-add-button"
+                            onClick={handleClickAdd}
+                        > 
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                            Tambah
+                        </button>
+                    </div>
+                </div>
                 <div className='grid grid-cols-4 gap-8'>
                 {activities.map(item=> {
                     const dateIndo = moment(item.created_at).format('LL');
@@ -30,6 +79,8 @@ const Activities = () => {
                             date={dateIndo}
                             id={item.id}
                             url={`/detail/${item.id}`}
+                            handleModal = {setShowModal}
+                            setIDActivity={setIDActivity}
                         />
                     )
                 })}
