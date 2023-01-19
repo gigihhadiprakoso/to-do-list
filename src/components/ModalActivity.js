f226e1b3000c4936c7621a28a842166a4b322d17
Gigih Hadi <hadigigih@gmail.com>
2023-01-20 00:25:11 +0700

    [ADD] todo-modal-delete

--- src/components/ModalActivity.js
+++ src/components/ModalActivity.js
@@ -30,7 +30,7 @@ const ModalActivity = (props) => {
 
     return (
         <>
-        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none" id="ModalDelete" data-cy="todo-modal-delete">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
 
                 {/*content*/}
